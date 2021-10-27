2d66d8cb48e4f8f00b550855a6203da601d74dc5
kozlov-92 <kozlov.an92@yandex.ru>
2021-10-27 20:07:41 +0300

    routing

--- src/app/App.js
+++ src/app/App.js
@@ -1,8 +1,21 @@
 import React from "react";
+import NavBar from "./components/navBar";
+import Login from "./components/login";
+import Main from "./components/main";
 import Users from "./components/users";
+import UserPage from "./components/userPage";
+import { Route } from "react-router-dom";
 
 function App() {
-    return <Users />;
+    return (
+        <>
+            <NavBar />
+            <Route exact path="/" component={Main} />
+            <Route path="/login" component={Login} />
+            <Route path="/users/userPage" component={UserPage} />
+            <Route exact path="/users" component={Users} />
+        </>
+    );
 }
 
 export default App;
